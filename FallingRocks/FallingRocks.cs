3625d0cd76477855299b6f8eb50eb92b285693ee
Georgi <124593129+GeorgiAtanasovA@users.noreply.github.com>
2024-08-20 19:35:17 +0300

    Add files via upload

---
+++ FallingRocks/FallingRocks.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace FallingRocks
+{
+   class FallingRocks : Print_GameOver
+   {
+      public class Rock
+      {
+         public int X;
+         public int Y;
+         public char Ch;
+         public ConsoleColor color;
+      }
+      public class Pad
+      {
+         public int X;
+         public int Y;
+         public string padSimbols = "<===>";
+      }
+      public static void PrintLineUnderTable()
+      {
+         int windowWidth = Convert.ToInt16(Console.WindowWidth - 2);
+         int lineUnderTable = Convert.ToInt16(Console.WindowHeight - 2);
+
+         Console.SetCursorPosition(1, lineUnderTable);
+         Console.WriteLine(new string('-', windowWidth));
+      }
+      private static void FallingRockCreatePrint(ref List<Rock> rocksList, ref char[] rocksArr, ref Random randGenerator, ref Pad pad)
+      {
+         //На всяка итерация се прави нов камък и се слага в листа с координати и се прнтира
+         //Взима се камък задават му се координати 
+         //и на всяко принтиране на листа с камъни първите са по надолу
+
+         Rock rock = new Rock();
+         int rockPosFromRocksArr = randGenerator.Next(0, rocksArr.Length);
+         rock.Ch = rocksArr[rockPosFromRocksArr];
+         rock.X = randGenerator.Next(0, Console.WindowWidth - 1);
+         rock.Y = 0;
+         rocksList.Add(rock);
+
+         for (int i = 0; i < rocksList.Count; i++)
+         {
+            rocksList[i].Y = rocksList[i].Y + 1;
+            if (rocksList[i].Y > Console.WindowHeight - 2) { rocksList.RemoveAt(i); }//In order not to overload the rocksList<>
+
+            //-------------------------------------------------------------------------------- 
+            else
+            {
+               //collision with table
+               if ((rocksList[i].Y == pad.Y) && ((rocksList[i].X >= pad.X && rocksList[i].X <= pad.X + pad.padSimbols.Length)) && (rocksList[i].Ch == '@'))
+               {
+                  Console.ForegroundColor = ConsoleColor.Green;
+                  rocksList[i].color = ConsoleColor.Green;
+                  Console.Beep(1100, 50);
+                  count++;
+
+                  //resize tablePad Upp
+                  if (pad.padSimbols.Length < 30)
+                  {
+                     string minimizeTabble = "<" + new string('=', pad.padSimbols.Length - 1);
+                     pad.padSimbols = minimizeTabble + ">";
+                  }
+               }
+               //resize tablePad Down
+               else if ((rocksList[i].Y == pad.Y) && ((rocksList[i].X >= pad.X && rocksList[i].X <= pad.X + pad.padSimbols.Length)) && (rocksList[i].Ch == '#'))
+               {
+                  string maximizeTabble = "<" + new string('=', pad.padSimbols.Length - 3);
+                  pad.padSimbols = maximizeTabble + ">";
+                  Console.ForegroundColor = ConsoleColor.Red;
+                  rocksList[i].color = ConsoleColor.Red;
+                  Console.Beep(300, 50);
+               }
+               else { Console.ForegroundColor = ConsoleColor.White; }
+               //--------------------------------------------------------------------------------
+
+               Console.SetCursorPosition(rocksList[i].X, rocksList[i].Y);
+               Console.Write(rocksList[i].Ch);
+            }
+         }
+       
+      }
+      static int count = 0;
+      static void Main(string[] args)
+      {
+         /// TO DO: 
+         /// set screen size +
+         /// set table moves +
+         /// set falling rocks +
+         /// set hit the table point +
+         /// set hit the table not point +
+         /// resize table +
+         
+         Console.WriteLine(" Start the game");
+         Console.WriteLine(" !!! Press left and right arrows to move ");
+         Console.ReadLine();
+         Console.Clear();
+
+         int speedTable = 1;
+         double divideScreen = 2;
+         Pad table = new Pad();
+         Print_GameOver gameOver = new Print_GameOver();
+         Console.WindowHeight = Convert.ToInt16(Console.LargestWindowHeight / divideScreen);
+         Console.WindowWidth = Convert.ToInt16(Console.LargestWindowWidth / divideScreen);
+         ConsoleKeyInfo pressedKey;
+         int left = Convert.ToInt16(Console.WindowWidth / divideScreen / divideScreen - 5);
+         int top = Convert.ToInt16(Console.WindowHeight - 3);
+         Console.BufferHeight = Convert.ToInt16(Console.LargestWindowHeight / divideScreen);
+         Console.BufferWidth = Convert.ToInt16(Console.LargestWindowWidth / divideScreen);
+
+         List<Rock> rocksList = new List<Rock>();
+         char[] rocksArr = { '@', '#', '%', '#', '#', '&', '☺', '+', '-'};
+         Random randGenerator = new Random();
+         Random randRocks = new Random();
+
+         //За началното принтиране
+         FallingRockCreatePrint(ref rocksList, ref rocksArr, ref randGenerator, ref table);
+         PrintLineUnderTable();
+         Console.SetCursorPosition(left, top);
+         Console.WriteLine(table.padSimbols);
+
+         while (true)
+         {
+            if (table.padSimbols.Length < 4) { Thread.Sleep(1500); gameOver.GameOver(); return; }
+            while (Console.KeyAvailable)
+            {
+               pressedKey = Console.ReadKey(true);
+               if (pressedKey.Key == ConsoleKey.LeftArrow) { if (left > 0) { left -= speedTable; } }
+               if (pressedKey.Key == ConsoleKey.RightArrow) { if (left < Console.WindowWidth - table.padSimbols.Length) { left += speedTable; } }
+            }
+            Console.WriteLine("    Score: " + count);
+            table.X = left;
+            table.Y = top;
+            Console.SetCursorPosition(left, top);
+            Console.WriteLine(table.padSimbols);
+
+            FallingRockCreatePrint(ref rocksList, ref rocksArr, ref randGenerator, ref table);
+            PrintLineUnderTable();
+
+            Thread.Sleep(160);
+            Console.Clear();
+         }
+      }
+   }
+}
---
+++ FallingRocks/PrintGameOver.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace FallingRocks
+{
+   class Print_GameOver
+   {
+      public void GameOver()
+      {
+         Console.Clear();
+         ConsoleKeyInfo stop;
+         List<char[,]> charsGameOver = new List<char[,]>();
+
+         #region  //Chars - writing Game Over 
+         char[,] charG = new char[7, 5]
+      {
+            {' ', 'G' , 'G' , 'G' , 'G' },
+            {'G', 'G' , ' ' , ' ' , 'G' },
+            {'G', ' ' , ' ' , ' ' , ' ' },
+            {'G', ' ' , ' ' , ' ' , ' ' },
+            {'G', ' ' , ' ' , 'G' , 'G' },
+            {'G', 'G' , ' ' , ' ' , 'G' },
+            {' ', 'G' , 'G' , 'G' , 'G' },
+      };
+         char[,] charA = new char[7, 7]
+ {
+            {' ', ' ' , ' ' , 'A' , ' ', ' ', ' '},
+            {' ', ' ' , 'A' , ' ' , 'A', ' ', ' '},
+            {' ', ' ' , 'A' , ' ' , 'A', ' ', ' '},
+            {' ', 'A' , ' ' , ' ' , ' ', 'A', ' '},
+            {' ', 'A' , 'A' , 'A' , 'A', 'A', ' '},
+            {'A', ' ' , ' ' , ' ' , ' ', ' ', 'A'},
+            {'A', ' ' , ' ' , ' ' , ' ', ' ', 'A'},
+ };
+         char[,] charM = new char[7, 7]
+{
+            {'M', ' ' , ' ' , ' ' , ' ', ' ', 'M'},
+            {'M', 'M' , ' ' , ' ' , ' ', 'M', 'M'},
+            {'M', ' ' , 'M' , ' ' , 'M', ' ', 'M'},
+            {'M', ' ' , ' ' , 'M' , ' ', ' ', 'M'},
+            {'M', ' ' , ' ' , ' ' , ' ', ' ', 'M'},
+            {'M', ' ' , ' ' , ' ' , ' ', ' ', 'M'},
+            {'M', ' ' , ' ' , ' ' , ' ', ' ', 'M'},
+};
+         char[,] charE = new char[7, 5]
+         {
+            {'E', 'E' , 'E' , 'E' , 'E' },
+            {'E', ' ' , ' ' , ' ' , ' ' },
+            {'E', ' ' , ' ' , ' ' , ' ' },
+            {'E', 'E' , 'E' , ' ' , ' ' },
+            {'E', ' ' , ' ' , ' ' , ' ' },
+            {'E', ' ' , ' ' , ' ' , ' ' },
+            {'E', 'E' , 'E' , 'E' , 'E' },
+         };
+         char[,] charSpace = new char[0, 0] { };
+
+         char[,] charO = new char[7, 5]
+          {
+            {' ', 'O' , 'O' , 'O' , ' ' },
+            {'O', 'O' , ' ' , 'O' , 'O' },
+            {'O', ' ' , ' ' , ' ' , 'O' },
+            {'O', ' ' , ' ' , ' ' , 'O' },
+            {'O', ' ' , ' ' , ' ' , 'O' },
+            {'O', 'O' , ' ' , 'O' , 'O' },
+            {' ', 'O' , 'O' , 'O' , ' ' },
+          };
+         char[,] charV = new char[7, 7]
+{
+            {'V', ' ' , ' ' , ' ' , ' ', ' ', 'V'},
+            {'V', ' ' , ' ' , ' ' , ' ', ' ', 'V'},
+            {' ', 'V' , ' ' , ' ' , ' ', 'V', ' '},
+            {' ', 'V' , ' ' , ' ' , ' ', 'V', ' '},
+            {' ', ' ' , 'V' , ' ' , 'V', ' ', ' '},
+            {' ', ' ' , 'V' , ' ' , 'V', ' ', ' '},
+            {' ', ' ' , ' ' , 'V' , ' ', ' ', ' '},
+};
+         char[,] charE2 = new char[7, 5]
+  {
+            {'E', 'E' , 'E' , 'E' , 'E' },
+            {'E', ' ' , ' ' , ' ' , ' ' },
+            {'E', ' ' , ' ' , ' ' , ' ' },
+            {'E', 'E' , 'E' , ' ' , ' ' },
+            {'E', ' ' , ' ' , ' ' , ' ' },
+            {'E', ' ' , ' ' , ' ' , ' ' },
+            {'E', 'E' , 'E' , 'E' , 'E' },
+  };
+         char[,] charR = new char[7, 5]
+  {
+            {'R', 'R' , 'R' , 'R' , ' ' },
+            {'R', ' ' , ' ' , ' ' , 'R' },
+            {'R', ' ' , ' ' , ' ' , 'R' },
+            {'R', 'R' , 'R' , 'R' , ' ' },
+            {'R', ' ' , ' ' , 'R' , ' ' },
+            {'R', ' ' , ' ' , ' ' , 'R' },
+            {'R', ' ' , ' ' , ' ' , 'R' },
+  };
+         char[,] charU = new char[7, 5]
+{
+            {' ', '!' , '!' , ' ' , ' ' },
+            {'!', '!' , '!' , '!' , ' ' },
+            {'!', '!' , '!' , '!' , ' ' },
+            {'!', '!' , '!' , '!' , ' ' },
+            {' ', '!' , '!' , ' ' , ' ' },
+            {' ', ' ' , ' ' , ' ' , ' ' },
+            {' ', '!' , '!' , ' ' , ' ' },
+};
+         #endregion
+
+         charsGameOver.Add(charG);
+         charsGameOver.Add(charA);
+         charsGameOver.Add(charM);
+         charsGameOver.Add(charE);
+         charsGameOver.Add(charSpace);
+         charsGameOver.Add(charO);
+         charsGameOver.Add(charV);
+         charsGameOver.Add(charE);
+         charsGameOver.Add(charR);
+         charsGameOver.Add(charU);
+
+         int consoleLength = 0;
+         foreach (var item in charsGameOver)
+         {
+            consoleLength += item.GetLength(1) + 1;
+         }
+
+         Console.WindowHeight = Convert.ToInt16(Console.LargestWindowHeight / 2);
+         Console.WindowWidth = Convert.ToInt16(consoleLength + 2);
+         Console.BufferHeight = Convert.ToInt16(Console.WindowHeight);
+         Console.BufferWidth = Convert.ToInt16(Console.WindowWidth);
+         int j = 0;
+         int i = 0;
+         int left = 0;
+         int space = 2;
+
+         foreach (var letter in charsGameOver)
+         {
+            int sound = 500;
+            for (i = 0; i < letter.GetLength(0); i++)
+            {
+               for (j = 0; j < letter.GetLength(1); j++)
+               {
+                  Console.SetCursorPosition(left + j + space, i + 8);
+                  Console.Write(letter[i, j]);
+               }
+               sound -= 60;
+               Console.Beep(sound, 80);
+               Console.WriteLine();
+            }
+            left += letter.GetLength(1); //Determines the spacing between the letters
+            space += 1; //Space between the letters
+         }
+         Console.WriteLine("\nPress 'space' to Exit!");
+         stop = Console.ReadKey();
+         if (stop.Key == ConsoleKey.Spacebar) { return; }
+
+         //Thread.Sleep(2000);
+      }
+   }
+}
